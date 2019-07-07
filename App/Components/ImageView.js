94e76fec96431ca6184a5fa67e8dc3dbb82f6366
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-07-07 16:05:17 +0700

    Data to seperate folder

--- App/Components/ImageView.js
+++ App/Components/ImageView.js
@@ -2,35 +2,10 @@ import React, { Component } from "react";
 import { Text, View, Dimensions, Image, ProgressViewIOS } from "react-native";
 import Carousel from "react-native-snap-carousel";
 import colors from "../Styles/colors";
+import returnData from "../Data/imageData";
 const sliderWidth = Dimensions.get("window").width;
-const dataDummy = [
-  {
-    name:
-      "https://media.gettyimages.com/photos/travel-woman-arms-outstretched-by-the-sea-picture-id638898418?s=2048x2048"
-  },
-  {
-    name:
-      "https://media1.tenor.com/images/ced3dc543f6db275e544f8d1fb3641e2/tenor.gif?itemid=9272787"
-  },
-  {
-    name:
-      "https://media1.tenor.com/images/01981ee970ee9409bb031d76d93f3a01/tenor.gif?itemid=8474279"
-  },
-
-  {
-    name:
-      "https://media1.tenor.com/images/8f7882f186fd49c25cd05f470247fe09/tenor.gif?itemid=9272799"
-  },
-  {
-    name:
-      "https://media1.tenor.com/images/452cea6cc5abcfbac66d1b62a18bacf5/tenor.gif?itemid=8474302"
-  },
-  {
-    name:
-      "https://media1.tenor.com/images/bdfecff3c30f96717f58ab477f544324/tenor.gif?itemid=8474276"
-  }
-];
-
+const data = returnData();
+console.log("data", data);
 class ImageView extends Component {
   constructor(props) {
     super(props);
@@ -43,17 +18,17 @@ class ImageView extends Component {
     return (
       <View style={{ paddingVertical: 15 }}>
         <Carousel
-          data={dataDummy}
+          data={data}
           itemWidth={sliderWidth - 30}
           sliderWidth={sliderWidth}
           removeClippedSubviews
           layout="default"
           onSnapToItem={index => {
             if (index == 0) {
-              newProgress = index / dataDummy.length;
+              newProgress = index / data.length;
             } else {
               index++;
-              newProgress = index / dataDummy.length;
+              newProgress = index / data.length;
             }
 
             this.setState({ progress: newProgress });
