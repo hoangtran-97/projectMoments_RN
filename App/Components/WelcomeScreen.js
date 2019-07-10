70d5b87d47456f83b43f929e547ffba634dd7cd6
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-07-10 16:37:18 +0700

    Welcome 1 complete

--- App/Components/WelcomeScreen.js
+++ App/Components/WelcomeScreen.js
@@ -5,7 +5,8 @@ import {
   TouchableOpacity,
   Modal,
   SafeAreaView,
-  TextInput
+  TextInput,
+  ScrollView
 } from "react-native";
 import colors from "../Styles/colors";
 import LottieView from "lottie-react-native";
@@ -33,55 +34,60 @@ class WelcomeScreen extends Component {
       <Modal animationType="slide" transparent={false} visible={true}>
         <SafeAreaView
           style={{
-            justifyContent: "center",
             flex: 1,
-            alignItems: "center",
             backgroundColor: colors.Orange
           }}
         >
-          <View>
-            <Text>Welcome to Moments</Text>
-          </View>
-          <View>
-            <TextInput
-              textAlign={"center"}
-              placeholder="Please enter your name"
-              placeholderTextColor={colors.CameraBlack}
-              numberOfLines={1}
-              onChangeText={note => this.onChangeNote(note)}
+          <ScrollView scrollEnabled={false}>
+            <View
               style={{
-                height: 80,
-                width: 180
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "100%"
               }}
-            />
-          </View>
-          <View style={{ height: 40 }}>
-            {this.state.note.length != 0 ? (
-              <View
+            >
+              <Text>Welcome to Moments</Text>
+              <TextInput
+                textAlign={"center"}
+                placeholder="Please enter your name"
+                placeholderTextColor={colors.CameraBlack}
+                numberOfLines={1}
+                onChangeText={note => this.onChangeNote(note)}
                 style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginTop: 40
+                  height: 80,
+                  width: 180
                 }}
-              >
-                <Text style={{ marginVertical: 40 }}>
-                  Hello {this.state.note}
-                </Text>
-                {this.state.showAnimated ? (
-                  <LottieView
-                    style={{ width: 180, height: 180 }}
-                    source={require("../assets/present.json")}
-                    autoPlay
-                    loop
-                  />
+              />
+
+              <View style={{ height: 40 }}>
+                {this.state.note.length != 0 ? (
+                  <View
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      marginTop: 40
+                    }}
+                  >
+                    <Text style={{ marginVertical: 40 }}>
+                      Hello {this.state.note}
+                    </Text>
+                    {this.state.showAnimated ? (
+                      <LottieView
+                        style={{ width: 180, height: 180 }}
+                        source={require("../assets/present.json")}
+                        autoPlay
+                        loop
+                      />
+                    ) : (
+                      <View style={{ height: 180 }} />
+                    )}
+                  </View>
                 ) : (
-                  <View style={{ height: 180 }} />
+                  <View style={{ height: 40 }} />
                 )}
               </View>
-            ) : (
-              <View style={{ height: 40 }} />
-            )}
-          </View>
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </Modal>
     );
