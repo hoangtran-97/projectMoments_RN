c5da6a9722aaf5ea5ea6971729f64da9d47493fa
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-12-20 10:33:49 +0200

    firebase upload working

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,10 +5,16 @@ import {
 
 import moment from "moment";
 import ImagePicker from "react-native-image-crop-picker";
+import firebase from "react-native-firebase";
+import uuid from "uuid/v4";
 import ImageView from "../components/ImageView";
 
 const MainScreen = () => {
-    const [imageData, setImageData] = useState([]);
+    const [imageData, setImageData] = useState([{
+        link:
+            "/private/var/mobile/Containers/Data/Application/B2BB4259-F556-4E8B-8770-129C51D49219/tmp/react-native-image-crop-picker/01206EDC-EC10-448D-AB69-2660C3294A8E.jpg",
+        time: "July 15th 2019"
+    }]);
     const [progress, setProgress] = useState(0);
     const [currentIndex, setCurrentIndex] = useState(0);
     useEffect(() => {
@@ -41,6 +47,29 @@ const MainScreen = () => {
             alert(e);
         });
     };
+    const uploadImage = () => {
+        const testData = {
+            link:
+                "/private/var/mobile/Containers/Data/Application/B2BB4259-F556-4E8B-8770-129C51D49219/tmp/react-native-image-crop-picker/01206EDC-EC10-448D-AB69-2660C3294A8E.jpg",
+            time: "July 15th 2019"
+        };
+        const ext = testData.link.split(".").pop();
+        const filename = `${uuid()}.${ext}`;
+        firebase
+            .storage()
+            .ref(`tutorials/images/${filename}`)
+            .putFile(testData.link)
+            .on(
+                firebase.storage.TaskEvent.STATE_CHANGED,
+                (snapshot) => {
+
+                },
+                (error) => {
+                    unsubscribe();
+                    alert("Sorry, Try again.", error);
+                }
+            );
+    };
     const getData = async () => {
         try {
             const imagesJSON = await AsyncStorage.getItem("imageData");
@@ -68,7 +97,7 @@ const MainScreen = () => {
                 progress={progress}
                 updateProgress={updateProgress}
             />
-            <Button onPress={() => clearAsyncStorage()} title="Clear" />
+            <Button onPress={() => uploadImage()} title="Clear" />
         </>
     );
 };
